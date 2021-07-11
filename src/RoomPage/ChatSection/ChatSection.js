b95306821b5f3d8a11c88ae6acb779d89e5e8b16
shobit <shobitnair10@gmail.com>
2021-07-11 20:17:19 +0530

    fix

--- src/RoomPage/ChatSection/ChatSection.js
+++ src/RoomPage/ChatSection/ChatSection.js
@@ -3,7 +3,8 @@ import Messages from "./Messages";
 import NewMessage from "./NewMessage";
 import {Grid} from "@material-ui/core"
 
-const ChatSection = () => {
+const ChatSection = (props) => {
+  let wid = (props.mobile)?"100vw":"20vw";
   return (
     <>
       <Grid item xs={12} id="chat_header">
@@ -12,7 +13,8 @@ const ChatSection = () => {
       <Grid item xs={12} id="chat_bubble_block">
         <Messages/>
       </Grid>
-      <Grid item xs={12} id="chat_input_block">
+      <Grid item xs={12} id="chat_input_block"
+      style={{"width":wid}}>
         <NewMessage/>
       </Grid>
     </>
